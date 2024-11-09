77ef4aa54bc1584683e308d6eebc8c54b80f9e89
root <root@ip-172-31-92-203.ec2.internal>
2024-11-09 22:03:46 +0000

    removed security group and changed count back to one each

--- main.tf
+++ main.tf
@@ -3,22 +3,20 @@ provider "aws" {
 }
 
 resource "aws_instance" "redhat_instance" {
-  count = 2
+  count = 1
   ami = "ami-0583d8c7a9c35822c"
   instance_type = "t2.micro"
   key_name = "TKP"
-  vpc_security_group_ids = [aws_security_group.Jenkins_setup.id]
   tags = {
     Name = "Carters-RHEL-instance" 
   }
 }
 
 resource "aws_instance" "ubuntu_instance" {
-  count = 2
+  count = 1
   ami = "ami-0866a3c8686eaeeba"
   instance_type = "t2.micro"
   key_name = "TKP"
-  vpc_security_group_ids = [aws_security_group.Jenkins_setup.id]
   tags = {
     Name = "Carters-Ubuntu-instance"
   }
