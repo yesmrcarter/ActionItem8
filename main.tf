7bdcf005a7512116aa94c4a42544c6d6d835e52a
root <root@ip-172-31-92-203.ec2.internal>
2024-11-09 22:08:29 +0000

    added one more resource to create push

--- main.tf
+++ main.tf
@@ -21,3 +21,12 @@ resource "aws_instance" "ubuntu_instance" {
     Name = "Carters-Ubuntu-instance"
   }
 }
+resource "aws_instance" "ubuntu_instance" {
+  count = 1
+  ami = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+  key_name = "TKP"
+  tags = {
+    Name = "Carters-Second-Ubuntu-instance"
+  }
+}
