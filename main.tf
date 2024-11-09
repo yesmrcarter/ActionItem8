68e6444c35ebe71d1d68bd42def8fa69e09ed91c
root <root@ip-172-31-92-203.ec2.internal>
2024-11-09 21:58:06 +0000

    increased instant count

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "redhat_instance" {
-  count = 1
+  count = 2
   ami = "ami-0583d8c7a9c35822c"
   instance_type = "t2.micro"
   key_name = "TKP"
@@ -14,7 +14,7 @@ resource "aws_instance" "redhat_instance" {
 }
 
 resource "aws_instance" "ubuntu_instance" {
-  count = 1
+  count = 2
   ami = "ami-0866a3c8686eaeeba"
   instance_type = "t2.micro"
   key_name = "TKP"
